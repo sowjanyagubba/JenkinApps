e96e618ed119f233276135e0e80008810115e807
sowjanyagubba <sowjanyagubba1223@gmail.com>
2022-11-15 12:19:51 +0530

    added new project

---
+++ Calculator/Cal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        public int Add(int x, int y)
+        {
+            return x + y
+    ;
+        }
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+    }
+
+}
